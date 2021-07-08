eb2c0a1eb0c75e132b02e9bc0a6eb55e10a96068
AHMED-ALMISALLAMY <Almisallamy2@icloud.com>
2021-07-08 13:37:30 +0200

    first commit

---
+++ oop.py
@@ -0,0 +1,20 @@
+class Car:
+
+    # Method
+    def __init__(self,name, model, price, color) -> None:
+        self.carname = name
+        self.modeldate = model
+        self.price = price
+        self.color = color
+    
+    def run(self):
+        print(f"The car name is: {self.carname}")
+        print(f"The car model is: {self.modeldate}")
+        print(f"The car price is: {self.price}")
+        print(f"The car's color is: {self.color}")
+    
+    
+# create object
+car_1 = Car("BMW",2017,200000,"blue")
+
+car_1.run()
